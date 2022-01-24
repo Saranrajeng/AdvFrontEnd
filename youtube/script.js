c2641bc0d39dfcf1abba534e30ced6cce82757e9
Saranrajeng <you@example.com>
2022-01-24 15:28:38 +0530

    finished

---
+++ youtube/script.js
@@ -0,0 +1,7 @@
+
+var search=document.getElementById("search");
+
+function get(){
+    var input=document.getElementById("input").value;
+    window.location.href=`https://www.youtube.com/results?search_query=${input}`
+}
